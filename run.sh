47164480d38748a3bd33b1e9ba9d76bb71d469c4
Chris Kaltwasser <chris@chrisk.com>
2023-09-16 18:27:53 +0000

    added run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while true; do
+    # Replace 'your_python_script.py' with the actual name of your Python
+    # program
+    echo Webtick
+    python src/websock-ticker.py 
+    echo Will try again....
+    sleep 30
+done
+
