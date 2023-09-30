7924630f534821e0ba9b50d453a1679a95d75abe
JCKalt <ckaltwasser@reminingcorp.com>
2023-09-30 11:14:16 -0400

    added changes

--- trading_system/trade_executor.py
+++ trading_system/trade_executor.py
@@ -1,25 +1,27 @@
-
 class TradeExecutor:
     def execute_trade(self, trade, balances, parameters):
-        print(f"Executing trade: {trade}")
+        try:
+            print(f"Executing trade: {trade}")
 
-        cycle, opportunity = trade
-        fee = opportunity * parameters['fee']
-        risk_measure = parameters['risk_measure']
+            cycle, opportunity = trade
+            fee = opportunity * parameters['fee']
+            risk_measure = parameters['risk_measure']
 
-        if cycle == 'cycle1':
-            required_btc = risk_measure * balances['BTC']
-            if balances['BTC'] >= required_btc:
-                balances['BTC'] -= required_btc
-                balances['BCH'] += required_btc * balances['BCHBTC']
-                balances['USD'] += balances['BCH'] * balances['BCHUSD']
-                balances['BTC'] += balances['USD'] / balances['BTCUSD']
-        elif cycle == 'cycle2':
-            required_btc = risk_measure * balances['BTC']
-            if balances['BTC'] >= required_btc:
-                balances['BTC'] -= required_btc
-                balances['USD'] += required_btc * balances['BTCUSD']
-                balances['BCH'] += balances['USD'] / balances['BCHUSD']
-                balances['BTC'] += balances['BCH'] * balances['BCHBTC']
+            if cycle == 'cycle1':
+                required_btc = risk_measure * balances['BTC']
+                if balances['BTC'] >= required_btc:
+                    balances['BTC'] -= required_btc
+                    balances['BCH'] += required_btc * balances['BCHBTC']
+                    balances['USD'] += balances['BCH'] * balances['BCHUSD']
+                    balances['BTC'] += balances['USD'] / balances['BTCUSD']
+            elif cycle == 'cycle2':
+                required_btc = risk_measure * balances['BTC']
+                if balances['BTC'] >= required_btc:
+                    balances['BTC'] -= required_btc
+                    balances['USD'] += required_btc * balances['BTCUSD']
+                    balances['BCH'] += balances['USD'] / balances['BCHUSD']
+                    balances['BTC'] += balances['BCH'] * balances['BCHBTC']
 
-        profit = opportunity - fee
+            profit = opportunity - fee
+        except Exception as e:
+            print(f"Error executing trade: {e}")
--- trading_system/trading_engine.py
+++ trading_system/trading_engine.py
@@ -1,18 +1,20 @@
-
 class TradingEngine:
     def make_decision(self, data, balances, parameters):
-        bchbtc_price = data['bchbtc'].get('price', 1)
-        bchusd_price = data['bchusd'].get('price', 1)
-        btcusd_price = data['btcusd'].get('price', 1)
-        btc_balance = balances['BTC']
+        try:
+            bchbtc_price = data['bchbtc'].get('price', 1)
+            bchusd_price = data['bchusd'].get('price', 1)
+            btcusd_price = data['btcusd'].get('price', 1)
+            btc_balance = balances['BTC']
 
-        opportunity1 = (btc_balance * bchbtc_price * bchusd_price) / btcusd_price - btc_balance
-        opportunity2 = (btc_balance / btcusd_price / bchusd_price) * bchbtc_price - btc_balance
+            opportunity1 = (btc_balance * bchbtc_price * bchusd_price) / btcusd_price - btc_balance
+            opportunity2 = (btc_balance / btcusd_price / bchusd_price) * bchbtc_price - btc_balance
 
-        print(f"Data: {data}, Opportunity1: {opportunity1}, Opportunity2: {opportunity2}")
+            print(f"Data: {data}, Opportunity1: {opportunity1}, Opportunity2: {opportunity2}")
 
-        if opportunity1 > parameters['arbitrage_opportunity_threshold']:
-            return 'cycle1', opportunity1
-        elif opportunity2 > parameters['arbitrage_opportunity_threshold']:
-            return 'cycle2', opportunity2
-        return None, None
+            if opportunity1 > parameters['arbitrage_opportunity_threshold']:
+                return 'cycle1', opportunity1
+            elif opportunity2 > parameters['arbitrage_opportunity_threshold']:
+                return 'cycle2', opportunity2
+            return None, None
+        except Exception as e:
+            print(f"Error making decision: {e}")
