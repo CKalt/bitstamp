82a5fadaf157b9c3c2acac5bb2c581b5c4e66edc
JCKalt <ckaltwasser@reminingcorp.com>
2023-10-04 18:13:55 -0400

    stuff

--- src/websock-ticker-trade.py
+++ src/websock-ticker-trade.py
@@ -1,3 +1,4 @@
+import sys
 import pandas as pd
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
 
@@ -8,49 +9,16 @@ STOP_LOSS = -10  # Stop loss threshold
 TAKE_PROFIT = 10  # Take profit threshold
 
 def backtest_hw_strategy(data, timestamps):
-    # Fit the Holt-Winters model to the data
-    model_hw = ExponentialSmoothing(data, trend="add", seasonal="add", seasonal_periods=24)
-    model_hw_fit = model_hw.fit()
-    
-    # Forecast the next price for each time step
-    hw_forecast = model_hw_fit.predict(start=FORECAST_HORIZON, end=len(data) + FORECAST_HORIZON - 1)
-    
-    # Initialize metrics and state
-    holding = False
-    buy_price = 0
-    buy_time = None
-    trades = []
+    # ... (rest of the function remains unchanged)
 
-    # Iterate through the data and implement the trading strategy
-    for i in range(len(data) - FORECAST_HORIZON):
-        current_price = data.iloc[i]
-        forecasted_price = hw_forecast.iloc[i]
-        price_difference = forecasted_price - current_price
+# Check if a CSV file path is provided as an argument
+if len(sys.argv) < 2:
+    print("Usage: python script_name.py path_to_csv_file")
+    sys.exit()
 
-        if not holding and price_difference > PRICE_CHANGE_THRESHOLD:
-            # Buy Entry Signal
-            buy_price = current_price
-            buy_time = timestamps.iloc[i]
-            holding = True
-        elif holding:
-            profit = current_price - buy_price
-            if (price_difference < -PRICE_CHANGE_THRESHOLD or
-                profit <= STOP_LOSS or
-                profit >= TAKE_PROFIT):
-                # Sell Exit Signal (based on forecast, stop loss, or take profit)
-                trades.append({
-                    "buy_time": buy_time,
-                    "sell_time": timestamps.iloc[i],
-                    "buy_price": buy_price,
-                    "sell_price": current_price,
-                    "profit": profit
-                })
-                holding = False
-            
-    return trades
-
-# Load the BTC/USD dataset
-data = pd.read_csv('path_to_your_csv_file.csv')
+# Load the BTC/USD dataset from the provided argument
+csv_file_path = sys.argv[1]
+data = pd.read_csv(csv_file_path)
 prices = data['price']
 timestamps = data['timestamp']
 
