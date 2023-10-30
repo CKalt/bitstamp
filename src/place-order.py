f77a412aa5b31ae2a739dd1d7f1b6317cb620cfd
JCKalt <ckaltwasser@reminingcorp.com>
2023-10-30 10:31:02 -0400

    stuff

--- src/place-order.py
+++ src/place-order.py
@@ -1,128 +1,169 @@
-#!env/bin/python
+#!/usr/bin/env python
 # src/place-orders.py
-from urllib.parse import urlencode
+
+import argparse
 import hashlib
 import hmac
-import time
-import requests
-import uuid
 import json
-import argparse
 import logging
-
-# Command line arguments
-parser = argparse.ArgumentParser(description='Process Bitstamp orders.')
-parser.add_argument('order_file', type=str, help='Order file')
-parser.add_argument('-v', '--verbose', action='store_true',
-                    help='Verbose output')
-args = parser.parse_args()
-
-# Read API configuration from file
-with open('.bitstamp', 'r') as f:
-    config = json.load(f)
-api_key = config['api_key']
-API_SECRET = bytes(config['api_secret'], 'utf-8')
-
-# Read order data from file
-with open(args.order_file, 'r') as f:
-    order = json.load(f)
-
-timestamp = str(int(round(time.time() * 1000)))
-nonce = str(uuid.uuid4())
-content_type = 'application/x-www-form-urlencoded'
-
-order_type = order.get('order_type', 'limit-buy')
-
-if order_type == 'limit-buy':
-    endpoint = '/api/v2/buy/'
-    payload = {'amount': str(order['amount']), 'price': str(order['price'])}
-elif order_type == 'limit-sell':
-    endpoint = '/api/v2/sell/'
-    payload = {'amount': str(order['amount']), 'price': str(order['price'])}
-elif order_type == 'market-buy' or order_type == 'instant-buy':
-    endpoint = '/api/v2/buy/market/'
-    # Reading the price from the input JSON
-    # Defaulting to '28113' if not provided
-    price_value = str(order.get('price', '28113'))
-    payload = {'amount': str(order['amount']), 'price': price_value}
-    # Outputting the constructed JSON payload for verification
-    print(f"Constructed Trade Payload: {json.dumps(payload)}")
-elif order_type == 'market-sell' or order_type == 'instant-sell':
-    endpoint = '/api/v2/sell/market/'
-    # Reading the price from the input JSON
-    # Defaulting to '28113' if not provided
-    price_value = str(order.get('price', '28113'))
-    payload = {'amount': str(order['amount']), 'price': price_value}
-    # Outputting the constructed JSON payload for verification
-    print(f"Constructed Trade Payload: {json.dumps(payload)}")
-else:
-    raise ValueError(f"Unsupported order type: {order_type}")
-
-url = 'https://www.bitstamp.net' + endpoint + order['currency_pair'] + '/'
+import time
+import uuid
+from urllib.parse import urlencode
+import requests
 
 
-payload_string = urlencode(payload)
+def read_config(file_name):
+    with open(file_name, 'r') as f:
+        return json.load(f)
 
-# '' (empty string) in message represents any query parameters or an empty string in case there are none
-message = 'BITSTAMP ' + api_key + \
-    'POST' + \
-    'www.bitstamp.net' + \
-    endpoint + order['currency_pair'] + '/' + \
-    '' + \
-    content_type + \
-    nonce + \
-    timestamp + \
-    'v2' + \
-    payload_string
-message = message.encode('utf-8')
-signature = hmac.new(API_SECRET, msg=message,
-                     digestmod=hashlib.sha256).hexdigest()
-headers = {
-    'X-Auth': 'BITSTAMP ' + api_key,
-    'X-Auth-Signature': signature,
-    'X-Auth-Nonce': nonce,
-    'X-Auth-Timestamp': timestamp,
-    'X-Auth-Version': 'v2',
-    'Content-Type': content_type
-}
 
-# Set up logging
-logging.basicConfig(filename='bitstamp.log', level=logging.INFO)
+def read_order_data(file_name):
+    with open(file_name, 'r') as f:
+        return json.load(f)
 
-url = 'https://www.bitstamp.net' + endpoint + order['currency_pair'] + '/'
-if args.verbose:
-    print(f"Request Method: POST")
-    print(f"Request URL: {url}")
-    print(f"Request Headers: {headers}")
-    print(f"Request Payload: {payload_string}")
-else:
-    logging.info(f"Request Method: POST")
-    logging.info(f"Request URL: {url}")
-    logging.info(f"Request Headers: {headers}")
-    logging.info(f"Request Payload: {payload_string}")
 
-r = requests.post(
-    url,
-    headers=headers,
-    data=payload_string
-)
+def create_payload(order):
+    order_type = order.get('order_type', 'limit-buy')
+    payload = {}
+    endpoint = ''
 
-if not r.status_code == 200:
-    print(f"Error Response from Bitstamp API: {r.text}")
-    logging.error(f"Unexpected status code: {r.status_code}")
-    logging.error(f"Response text: {r.text}")
-    raise Exception('Status code not 200')
+    if order_type in ['limit-buy', 'limit-sell']:
+        endpoint = f"/api/v2/{'buy' if order_type == 'limit-buy' else 'sell'}/"
+        payload = {'amount': str(order['amount']),
+                   'price': str(order['price'])}
+    elif order_type in ['market-buy', 'instant-buy', 'market-sell', 'instant-sell']:
+        endpoint = f"/api/v2/{'buy' if order_type in ['market-buy', 'instant-buy'] else 'sell'}/market/"
+        price_value = str(order.get('price', '28113'))
+        payload = {'amount': str(order['amount']), 'price': price_value}
+        print(f"Constructed Trade Payload: {json.dumps(payload)}")
+    else:
+        raise ValueError(f"Unsupported order type: {order_type}")
 
-string_to_sign = (nonce + timestamp +
-                  r.headers.get('Content-Type')).encode('utf-8') + r.content
-signature_check = hmac.new(
-    API_SECRET, msg=string_to_sign, digestmod=hashlib.sha256).hexdigest()
-if not r.headers.get('X-Server-Auth-Signature') == signature_check:
-    logging.error('Signatures do not match')
-    raise Exception('Signatures do not match')
+    return payload, endpoint
 
-if args.verbose:
-    print(f"Response: {r.content}")
-else:
-    logging.info(f"Response: {r.content}")
-print(r.content)
+
+def create_message(api_key, endpoint, currency_pair, content_type, nonce, timestamp, payload_string):
+    return f"BITSTAMP {api_key}POSTwww.bitstamp.net{endpoint}{currency_pair}/{content_type}{nonce}{timestamp}v2{payload_string}"
+
+
+def setup_logging():
+    logging.basicConfig(filename='bitstamp.log', level=logging.INFO)
+
+
+def fetch_order_status(api_key, API_SECRET, order_id):
+    endpoint = "/api/v2/order_status/"
+    content_type = 'application/x-www-form-urlencoded'
+    timestamp = str(int(round(time.time() * 1000)))
+    nonce = str(uuid.uuid4())
+
+    payload = {'id': order_id}
+    message = create_message(
+        api_key, endpoint, '', content_type, nonce, timestamp, urlencode(payload))
+
+    signature = hmac.new(API_SECRET, msg=message.encode(
+        'utf-8'), digestmod=hashlib.sha256).hexdigest()
+    headers = {
+        'X-Auth': f'BITSTAMP {api_key}',
+        'X-Auth-Signature': signature,
+        'X-Auth-Nonce': nonce,
+        'X-Auth-Timestamp': timestamp,
+        'X-Auth-Version': 'v2',
+        'Content-Type': content_type
+    }
+
+    r = requests.post(
+        f"https://www.bitstamp.net{endpoint}", headers=headers, data=urlencode(payload))
+
+    if r.status_code == 200:
+        return r.json()
+    else:
+        logging.error(f"Error fetching order status: {r.text}")
+        return None
+
+
+def main():
+    # Command line arguments
+    parser = argparse.ArgumentParser(description='Process Bitstamp orders.')
+    parser.add_argument('order_file', type=str, help='Order file')
+    parser.add_argument('-v', '--verbose',
+                        action='store_true', help='Verbose output')
+    args = parser.parse_args()
+
+    # Read configurations
+    config = read_config('.bitstamp')
+    api_key = config['api_key']
+    API_SECRET = bytes(config['api_secret'], 'utf-8')
+    order = read_order_data(args.order_file)
+
+    timestamp = str(int(round(time.time() * 1000)))
+    nonce = str(uuid.uuid4())
+    content_type = 'application/x-www-form-urlencoded'
+    payload, endpoint = create_payload(order)
+    message = create_message(
+        api_key, endpoint, order['currency_pair'], content_type, nonce, timestamp, urlencode(payload))
+
+    signature = hmac.new(API_SECRET, msg=message.encode(
+        'utf-8'), digestmod=hashlib.sha256).hexdigest()
+    headers = {
+        'X-Auth': f'BITSTAMP {api_key}',
+        'X-Auth-Signature': signature,
+        'X-Auth-Nonce': nonce,
+        'X-Auth-Timestamp': timestamp,
+        'X-Auth-Version': 'v2',
+        'Content-Type': content_type
+    }
+
+    setup_logging()
+
+    url = f"https://www.bitstamp.net{endpoint}{order['currency_pair']}/"
+    if args.verbose:
+        print(f"Request Method: POST")
+        print(f"Request URL: {url}")
+        print(f"Request Headers: {headers}")
+        print(f"Request Payload: {urlencode(payload)}")
+    else:
+        logging.info(f"Request Method: POST")
+        logging.info(f"Request URL: {url}")
+        logging.info(f"Request Headers: {headers}")
+        logging.info(f"Request Payload: {urlencode(payload)}")
+
+    r = requests.post(url, headers=headers, data=urlencode(payload))
+
+    if r.status_code != 200:
+        error_message = f"Error Response from Bitstamp API: {r.text}"
+        print(error_message)
+        logging.error(f"Unexpected status code: {r.status_code}")
+        logging.error(error_message)
+        raise Exception('Status code not 200')
+
+    string_to_sign = f"{nonce}{timestamp}{r.headers.get('Content-Type')}".encode(
+        'utf-8') + r.content
+    signature_check = hmac.new(
+        API_SECRET, msg=string_to_sign, digestmod=hashlib.sha256).hexdigest()
+
+    if r.headers.get('X-Server-Auth-Signature') != signature_check:
+        logging.error('Signatures do not match')
+        raise Exception('Signatures do not match')
+
+    if args.verbose:
+        print(f"Response: {r.content}")
+    else:
+        logging.info(f"Response: {r.content}")
+
+    print(r.content)
+
+    order_response = json.loads(r.content.decode('utf-8'))
+    if 'id' in order_response:
+        order_id = order_response['id']
+
+        for i in range(5):
+            time.sleep(1)
+            status = fetch_order_status(api_key, API_SECRET, order_id)
+            if status:
+                print(json.dumps(status, indent=4))
+    else:
+        print("Order ID not found in response. Can't fetch status.")
+
+
+if __name__ == '__main__':
+    main()
