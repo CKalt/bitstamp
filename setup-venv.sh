51ac50537b8b0da798906852e904da1dcfb2a168
Chris Kaltwasser <chris@chrisk.com>
2024-12-05 14:52:31 +0000

    setup stuff

--- setup-venv.sh
+++ setup-venv.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+# Create virtual environment using Python 3.11
 python3.11 -m venv env
+
+# Activate the virtual environment
 source env/bin/activate
-pip install --upgrade pip
+
+# Upgrade pip and setuptools
+pip install --upgrade pip setuptools
 
 # Install numpy first to satisfy statsmodels build requirements
 pip install numpy==1.24.3
